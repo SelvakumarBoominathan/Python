9e618657fa53d994042040401e0e3a8fc5178c3d
Selvakumar Boominathan <selvans2k@gmail.com>
2025-12-30 21:19:48 +0530

    coaster_ride_ticket addition of age check

--- coaster_ride_ticket.py
+++ coaster_ride_ticket.py
@@ -4,6 +4,16 @@ def ticket_calculator():
 
     if height >= 120:
       print("Great!. you are eligible to ride the roller coaster.")
+      age = int(input("What is your age? "))
+      if age>=12 and age <=18:
+        print("Your ticket price is $7.")
+      elif age < 12 and age >=4:
+        print("Your ticket price is $5.")
+      elif age > 18:
+        print("Your ticket price is $12.") 
+      else:
+        print("Your ticket price is $0.")
+
     else:
         print("Sorry, you have to be at least 120 cm to ride the roller coaster.")
         return
