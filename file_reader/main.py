17488ff31c148f1c64a7a502010d1538f61adb4d
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-05 23:29:06 +0530

    smaple push

--- file_reader/main.py
+++ file_reader/main.py
@@ -1,8 +1,21 @@
+# work with normal csv library from Python standard library
+
 import csv
 
 with open("sample_employee_data.csv") as file:
-  data = csv.reader(file)
-  experience = []
-  for row in data:
-    experience.append(row[4])
-  print(experience)
+   data = csv.reader(file)
+   experience_of_employees = []
+   
+   for row in data:
+      if row[4] != "Experience_Years":
+        experience_of_employees.append(int(row[4]))
+
+   print(experience_of_employees)
+
+
+# import pandas as pd
+
+# data = pd.read_csv("sample_employee_data.csv")
+
+# print(data)
+       
