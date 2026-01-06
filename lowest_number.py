beab5834541e156126f371ffb0dc6a4044d1f5a6
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-06 23:31:25 +0530

    DSA approach added

--- lowest_number.py
+++ lowest_number.py
@@ -10,3 +10,18 @@ def lowest_num(arr):
 
 
 print(lowest_num(arr))
+
+
+# DSA approach
+
+
+# arr = [4,3,5,6,33,7,22,90,1]
+
+# def lowest_num(arr):
+#     lowest = arr[0]
+#     for num in arr[1:]:   #start from second element
+#         if num < lowest:
+#             lowest = num
+#     return lowest
+
+# print(lowest_num(arr))
