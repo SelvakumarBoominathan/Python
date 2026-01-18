9f134e1d5cff567f087e7e3fc9b9082983e00ec4
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-18 15:40:59 +0530

    added few methods to list

--- list_methods.py
+++ list_methods.py
@@ -1,6 +1,21 @@
-mylist = ["apple", "banana", "cherry"]
+mylist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 
 
 # List Length
-
 print(f"Length of List is : {len(mylist)}")
+
+# list type
+print(f"Type of List is : {type(mylist)}")
+
+
+# list constructor
+print(f"List Constructor : {list(('apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango', 'grapes'))}")
+
+# access list items
+mylist[0] = "orange"
+print(f"Access List Item : {mylist[0]}")
+print(f"Access List Item : {mylist[1:3]}")
+print(f"Access List Item : {mylist[-1]}")   # negative indexing
+print(f"Access List Item : {mylist[-3:-1]}") # negative indexing
+print(f"Access List Item : {mylist[:2]}")   # slicing  - from beginning to index 3 omits last index
+print(f"Access List Item : {mylist[1:]}")    # slicing
