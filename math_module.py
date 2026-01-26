ebdbed06eba5470683c29b8db2bc6890cab68ed8
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-26 20:18:08 +0530

    added math methods in functions

---
+++ math_module.py
@@ -0,0 +1,24 @@
+# Python has build in math module in it.
+
+# functions from math module
+
+
+import math
+arr = [16, 25, 36, 49, 64]
+
+print(min(arr))  # returns the minimum value from the array
+
+print(max(arr))  # returns the maximum value from the array
+
+print(abs(-7.25))  # returns the absolute value
+
+print(pow(4, 3))  # returns the power of a number
+
+
+# methods and constants from math module (need to import math module to access these methods and constants)
+
+print(math.sqrt(54))  # returns the square root of a number
+
+print(math.floor(5.67))  # returns the largest integer less than or equal to a number
+
+print(math.ceil(5.67))  # returns the smallest integer greater than or equal to a number
