62cbda299242d1795a8bb9c35732be291802f441
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-28 23:05:34 +0530

    added recursion concept

---
+++ reursion_in_python/recursion.py
@@ -0,0 +1,9 @@
+
+# Recursion: A function that calls itself to solve a smaller instance of the same problem.
+def fibonacci(n):
+  if n<=1:
+    return n
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
+  
+print(fibonacci(6))
