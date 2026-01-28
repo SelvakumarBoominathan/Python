b38c6daf7a0b3cf8dede4f122d80aef249880719
Selvakumar Boominathan <selvans2k@gmail.com>
2026-01-29 01:23:12 +0530

    self is added

--- class_in_python/class_eg.py
+++ class_in_python/class_eg.py
@@ -1,12 +1,12 @@
 class Sample:
-
+    
     def __init__(self, a, b):
         self.a = a
         self.b = b
-
+    
     def addition(self):
         print(f"Addtion is : {self.a + self.b}")
-
+        
     def subtraction(self):
         print(f"subtraction is : {self.a - self.b}")
 
@@ -15,7 +15,7 @@ class Sample:
 
     def division(self):
         print(f"division is : {self.a / self.b}")
-
+        
 
 test = Sample(20, 29)
 
