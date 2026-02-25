bf0271429b1ee56b2ab890b84d3a3ee901b0f0da
Selvakumar Boominathan <selvans2k@gmail.com>
2026-02-25 18:42:18 +0530

    Added all the Linked list methods

--- DSA in Python/Two Pointers/Linkedlist/linkedlist.py
+++ DSA in Python/Two Pointers/Linkedlist/linkedlist.py
@@ -2,21 +2,19 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
-        
+
 
 class Linked_list:
-    
-     
+
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
-        
-    def append(self,value):
+
+    def append(self, value):
         new_node = Node(value)
-        
+
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -24,38 +22,38 @@ class Linked_list:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-    
+        return True
+
     def print_list(self):
         temp = self.head
         while temp:
             if temp.value is not None:
                 print(temp.value)
             temp = temp.next
-    
+
     def pop_from_list(self):
-        
+
         if self.head == None:
             return "List is empty already."
-      
+
         temp = self.head
         pre = self.head
         while temp.next:
             pre = temp
             temp = temp.next
-    
+
         self.tail = pre
         self.tail.next = None
         self.length -= 1
         if self.length == 0:
             self.head = None
             self.tail = None
-        
+
         return temp.value
-    
-    
+
     def prepend(self, value):
         new_node = Node(value)
-        
+
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
@@ -64,44 +62,90 @@ class Linked_list:
             self.head = new_node
         self.length += 1
         return True
-    
+
     def popfirst(self):
         if self.length == 0:
-            return "List is empty already"
+            print("List is empty already")
+            return False
         temp = self.head
         self.head = self.head.next
         temp.next = None
         self.length -= 1
         if self.length == 0:
             self.tail = None
-        return temp.value
-            
-            
-            
+        return temp
+
     def get(self, index):
-        if index <0 or index >= self.length:
-            print("List Index Out of Range")
-            return
+        if index < 0 or index > self.length:
+            return None
+
         temp = self.head
-        
+
         for _ in range(index):
             temp = temp.next
-            
-            
-        
-            
-        
-            
-              
-My_Linked_List = Linked_list("Selva")
-My_Linked_List.append(13)
-#My_Linked_List.append(20)
+        return temp
+
+    def insert_value(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.popfirst()
+        if index == self.length-1:
+            return self.pop_from_list()
+
+        pre = self.get(index-1)
+        temp = pre.next
+
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        print(f"Removed Item {temp.value}")
+        return temp
+
+
+My_Linked_List = Linked_list(11)
+My_Linked_List.append(3)
+My_Linked_List.append(23)
+My_Linked_List.append(7)
+print("Current length of LL : ", My_Linked_List.length)
 My_Linked_List.print_list()
-#print("Popped item:", My_Linked_List.pop_from_list())
-print(My_Linked_List.length)
-print(My_Linked_List.pop_from_list())
-#My_Linked_List.prepend("Boomi")
-print(My_Linked_List.popfirst())
-print(My_Linked_List.length)
+print("_____####_____")
+My_Linked_List.remove(2)
+
+My_Linked_List.print_list()
+print("Current length of LL : ", My_Linked_List.length)
 
 
+# print(My_Linked_List.get(1))
+# My_Linked_List.set_value(1, 400)
+
+# print("Current length of LL : ",My_Linked_List.length)
+# My_Linked_List.print_list()
+# print("Popped item:", My_Linked_List.pop_from_list())
+# print(My_Linked_List.length)
+# print(My_Linked_List.pop_from_list())
+# My_Linked_List.prepend("Boomi")
+# print(My_Linked_List.popfirst())
+# print(My_Linked_List.length)
