61ec84404a89e321576a99f9460447c63ff13048
Selvakumar Boominathan <selvans2k@gmail.com>
2026-03-04 19:49:15 +0530

    series and location properties

--- Pandas/Pandas.py
+++ Pandas/Pandas.py
@@ -1,11 +1,22 @@
+# import pandas as pd
+
+
+# print("Pandas version:", pd.__version__)
+
+# # Sample Series from a list
+# my_list = ["10", "20", "30", "40", "50"]
+
+# my_series = pd.Series(my_list)
+
+# print(my_series)
+
 import pandas as pd
 
 
-print("Pandas version:", pd.__version__)
+my_list = [10, 20, 30, 40, 50]
 
-# Sample Series from a list
-my_list = [10, 20, 30, 40, 50, "Selva"]
+index_list = ['a', 'b', 'c', 'd', 'e']
 
-my_series = pd.Series(my_list)
+my_series = pd.Series(my_list, index=index_list)
 
-print(my_series)
+print(my_series.loc["c"])
