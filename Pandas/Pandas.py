141e44702ecc84dabae07d3f94750d61b8833915
Selvakumar Boominathan <selvans2k@gmail.com>
2026-03-04 22:27:45 +0530

    text data added

--- Pandas/Pandas.py
+++ Pandas/Pandas.py
@@ -9,19 +9,36 @@ index_list = ['a', 'b', 'c', 'd', 'e']
 my_series = pd.Series(my_list, index=index_list)
 
 # Assigning a new value to an element using loc label
-#my_series.loc["c"] = 1000
+my_series.loc["c"] = 1000
 
-#print(my_series)
+print(my_series)
 
 
 # Accessing elements using index labels
-#print(my_series.loc["c"])
+print(my_series.loc["c"])
 
 # Accessing elements using integer position
-#print(my_series.iloc[1])
+print(my_series.iloc[1])
 
-print(my_series[my_series >=40])
-
-
+print(my_series[my_series < 40])
 
 
+# series constructor with dictonaries (key value pairs)
+
+my_dict = {"Day1": 1065, "Day2": 12070, "Day3": 2000, "Day4": 1500}
+
+my_calorie_tracker = pd.Series(my_dict)
+
+
+print(my_calorie_tracker[my_calorie_tracker == 2000])
+
+
+# Creating dataframe from a dictionary of lists
+
+my_dict = {"Name": ["Alice", "Bob", "Charlie"],
+           "Age": [25, 30, 35],
+           "City": ["New York", "Los Angeles", "Chicago"]}
+
+my_dataframe = pd.DataFrame(my_dict, index=["Person1", "Person2", "Person3"])
+
+print(my_dataframe)
