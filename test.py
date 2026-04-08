4b66f2d3abc089ffb4f55241dd7524daf836683b
Selvakumar Boominathan <selvans2k@gmail.com>
2026-04-08 23:47:36 +0530

    Test run a python sample code

---
+++ test.py
@@ -0,0 +1,8 @@
+import openpyxl
+
+work_book = openpyxl.load_workbook(
+    "C:\\Users\\selva\\OneDrive\\Desktop\\testfilepandas\\New Microsoft Excel Worksheet.xlsx")
+
+sheet_obj = work_book['Answers']
+
+print(sheet_obj['B2'].value)
